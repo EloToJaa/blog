1d6897ec0a428fe978ef43d7f0f8c8e6bfde0577
EloToJa <lukaszbudziak122@gmail.com>
2023-03-08 01:30:39 +0100

    user profile update

--- src/routes/profile/[username]/+page.svelte
+++ src/routes/profile/[username]/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
 	import Posts from '$lib/components/Posts/Posts.svelte';
+	import getDate from '$lib/utils/getDate';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 	const user = data.user;
 	const posts = data.posts;
 </script>
+
+<svelte:head>
+	<title>{user.username}</title>
+</svelte:head>
+
+<h1>{user.username}</h1>
+<h4>Joined at {getDate(user.created)}</h4>
 
 <Posts {posts} author={user.username} />
