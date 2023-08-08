b61f0fb7d299f4944505659671dc53a7667b5c61
EloToJa <elotoja@protonmail.com>
2023-08-08 20:56:23 +0200

    Create base layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+    fontFamily: {
+      main: ["'Fira Code'", "monospace"],
+    }
+  },
+  darkMode: "class",
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+    ]
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
