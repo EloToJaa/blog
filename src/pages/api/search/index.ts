864e081e68547cacf063d7a839e2d33aedb0eb88
EloToJa <elotoja@protonmail.com>
2023-12-13 23:44:30 +0100

    Move fuse out of endpoint

--- src/pages/api/search/index.ts
+++ src/pages/api/search/index.ts
@@ -4,22 +4,22 @@ import Fuse from "fuse.js";
 
 export const prerender = false;
 
+const blogCollection = new BlogCollection();
+await blogCollection.getCollection();
+
+const fuse = new Fuse(blogCollection.getPosts(), {
+  keys: [
+    "data.title",
+    "slug",
+    "data.author.slug",
+    "data.pubDatetime",
+    "data.tags",
+    "data.description",
+  ],
+  threshold: 0.6,
+});
+
 export const GET: APIRoute = async ({ url }) => {
-  const blogCollection = new BlogCollection();
-  await blogCollection.getCollection();
-
-  const fuse = new Fuse(blogCollection.getPosts(), {
-    keys: [
-      "data.title",
-      "slug",
-      "data.author.slug",
-      "data.pubDatetime",
-      "data.tags",
-      "data.description",
-    ],
-    threshold: 0.6,
-  });
-
   const searchPhrase = url.searchParams.get("q") || "";
   const limit = Math.min(
     parseInt(url.searchParams.get("limit") || "5", 10),
