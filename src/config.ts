a47345f2fc3717bd81138e9226f95a9ba2c4bb16
EloToJa <elotoja@protonmail.com>
2023-12-14 06:14:20 +0100

    Move navbar config

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+export const navbar = [
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "About",
+    url: "/about",
+  },
+  {
+    title: "Contact",
+    url: "/contact",
+  },
+];
