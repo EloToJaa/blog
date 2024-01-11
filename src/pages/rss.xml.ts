0489911c3f117376f43d9d98c749f2c33d78ef14
elotoja <lukasz.budziak@masters.com.pl>
2024-01-11 11:34:13 +0100

    Add rss.xml

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,24 @@
+import rss, { type RSSFeedItem } from "@astrojs/rss";
+import BlogCollection from "@utils/blog";
+import type { APIContext } from "astro";
+
+export async function GET(context: APIContext) {
+  const blogCollection = new BlogCollection();
+  await blogCollection.getCollection();
+  const posts = blogCollection.getPosts();
+  console.log(posts);
+
+  return rss({
+    title: "EloToJa's Blog",
+    description: "A blog about web development and other stuff",
+    // https://docs.astro.build/en/reference/api-reference/#contextsite
+    site: context.site ?? "",
+    items: posts.map(post => ({
+      title: post.data.title,
+      description: post.data.description,
+      link: `/blog/${post.slug}`,
+      pubDate: post.data.pubDatetime,
+    })) as RSSFeedItem[],
+    customData: `<language>en-us</language>`,
+  });
+}
