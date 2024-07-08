8d746ca013ab44ca1295b5ea358f48a9984260be
EloToJa <elotoja@protonmail.com>
2024-07-08 20:33:46 +0200

    Update layout & look

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,7 +12,26 @@ module.exports = {
     themes: [
       {
         dark: {
-          ...require("daisyui/src/theming/themes")["night"],
+          primary: "#008aff",
+          "primary-content": "#000716",
+          secondary: "#00a100",
+          "secondary-content": "#000900",
+          accent: "#0084d1",
+          "accent-content": "#000610",
+          neutral: "#1e151d",
+          "neutral-content": "#cdcacc",
+          "base-100": "#262626",
+          "base-200": "#202020",
+          "base-300": "#191919",
+          "base-content": "#cfcfcf",  
+          info: "#0094c2",   
+          "info-content": "#00080e", 
+          success: "#00e0ab",   
+          "success-content": "#00120b",   
+          warning: "#ffc900",
+          "warning-content": "#160f00",
+          error: "#ed0030",    
+          "error-content": "#140001",
         },
         light: {
           ...require("daisyui/src/theming/themes")["fantasy"],
